257538888504e9c9ef8768c150fdcc25f60fc30c
SquirrelLeonid <rm200002@yandex.ru>
2022-01-10 20:24:50 +0500

    Implemented dice roller

---
+++ BotGames/Thousand/DiceRoller.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BotGames.Thousand
+{
+    public class DiceRoller
+    {
+        private readonly Random random;
+
+        public DiceRoller()
+        {
+            random = new Random();
+        }
+
+        public int RollNextDice()
+        {
+            return random.Next(1, 6);
+        }
+
+        public IEnumerable<int> RollNextDices(int diceNumber)
+        {
+            for (var i = 0; i < diceNumber; i++)
+                yield return RollNextDice();
+        }
+    }
+}
